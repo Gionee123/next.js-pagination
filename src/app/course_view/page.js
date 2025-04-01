76b2cc1a9da439dbdc3b09dd72a5f28403789121
naveen <naveensainijpr@gmail.com>
2025-04-01 22:34:04 +0530

    fist

--- src/app/course_view/page.js
+++ src/app/course_view/page.js
@@ -2,14 +2,10 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
-import { FaEdit, FaTrash } from "react-icons/fa";
-import Link from "next/link";
-import { useParams } from "next/navigation";
 
 export default function Course_view() {
     const [courses, setCourses] = useState([]);
-    const [deleteplusstatuschange, setdeleteplusstatuschange] = useState(false); //delete aur status change  both same use
-    const [multiidsget, setmultiidsget] = useState([]); // mutiple ids get fors multiple delete
+
     const [pageNumber, setPageNumber] = useState(1); // वर्तमान पेज नंबर को ट्रैक करता है (शुरुआत में 1)
     const [totalLen, setTotalLen] = useState(0); // कुल रिकॉर्ड की संख्या को स्टोर करता है
     const [limit, setLimit] = useState(5); //प्रत्येक पेज पर कितने रिकॉर्ड दिखाने हैं
@@ -32,7 +28,7 @@ export default function Course_view() {
     };
     useEffect(() => {
         axios
-            .post("http://localhost:5000/api/backend/courses/view", {
+            .post("https://node-js-pagination-pyo7.onrender.com/api/backend/courses/view", {
                 page: pageNumber,
                 limit: limit,
             })
@@ -48,117 +44,15 @@ export default function Course_view() {
             .catch((err) => {
                 setError(err.message);
             });
-    }, [deleteplusstatuschange, pageNumber, limit]); // जब यह वैल्यू बदलेगी तो API कॉल होगी
-
-    // delete api
-    let deletecourse = (id) => {
-        if (!window.confirm("Are you sure you want to delete this course?")) return;
-
-        let data = {
-            id: id,
-        };
-        console.log("data", data);
-
-        axios
-            .post("http://localhost:5000/api/backend/courses/delete", data)
-            .then((response) => {
-                if (response.data.status == true) {
-                    setdeleteplusstatuschange(!deleteplusstatuschange);
-                    toast.success("Course deleted successfully!");
-                } else {
-                    toast.error(response.data.message);
-                }
-            })
-            .catch((err) => {
-                console.error("Delete Error:", err.message);
-                toast.error("Something went wrong while deleting!");
-            });
-    };
-    //statuschange y api
-    let statuschange = (id, status) => {
-        let data = {
-            id: id,
-            status: !status, // !status इसका मतलब है कि अगर कोर्स true (Active) है, तो false (Inactive) कर देंगे, और अगर false है, तो true कर देंगे।
-        };
-        console.log("data", status);
-
-        axios
-            .put("http://localhost:5000/api/backend/courses/change-status", data)
-            .then((response) => {
-                if (response.data.status == true) {
-                    setdeleteplusstatuschange(!deleteplusstatuschange);
-                    toast.success("Course status updated successfully!");
-                } else {
-                    toast.error(response.data.message);
-                }
-            })
-            .catch((err) => {
-                console.error("Delete Error:", err.message);
-                toast.error("Something went wrong while updating status!");
-            });
-    };
-
-    //multidelete ke liye pehal multiselec karna padga
-    let multiselect = (id) => {
-        let updatemultipleIds = [...multiidsget]; // मौजूदा IDs की कॉपी बनाएं
-        console.log(updatemultipleIds);
-        if (updatemultipleIds.includes(id)) {
-            // अगर ID पहले से सेलेक्टेड है, तो उसे लिस्ट से हटा दें
-            updatemultipleIds = updatemultipleIds.filter((value) => value !== id);
-        } else {
-            // अगर ID सेलेक्टेड नहीं है, तो उसे लिस्ट में जोड़ें
-            updatemultipleIds.push(id);
-        }
-        setmultiidsget(updatemultipleIds); // स्टेट अपडेट करें
-    };
-    let multideletecourse = () => {
-        if (multiidsget.length === 0) {
-            toast.error("Please select at least one course to delete!");
-            return;
-        }
-
-        if (!window.confirm("Are you sure you want to delete selected courses?"))
-            return;
-        let data = {
-            ids: multiidsget,
-        };
-        console.log("data", data);
-
-        axios
-            .post("http://localhost:5000/api/backend/courses/multiple-delete", data)
-            .then((response) => {
-                if (response.data.status == true) {
-                    setdeleteplusstatuschange(!deleteplusstatuschange);
-                    toast.success("Course deleted successfully!");
-                    setmultiidsget([]); // ✅ Deleted IDs Clear करें
-                } else {
-                    toast.error(response.data.message);
-                }
-            })
-            .catch((err) => {
-                console.error("Delete Error:", err.message);
-                toast.error("Something went wrong while deleting!");
-            });
-    };
-
+    }, [pageNumber, limit]); // जब यह वैल्यू बदलेगी तो API कॉल होगी
     return (
         <div className="flex justify-center">
             <ToastContainer position="top-right" autoClose={100} />
 
-            <div className="w-full max-w-6xl">
+            <div className="w-full p-[12px]">
                 <div className="bg-white shadow overflow-hidden rounded-lg">
-                    <button
-                        className={`bg-red-600 hover:bg-red-700 text-white px-4 py-2 text-lg rounded-md shadow-lg transition-all duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center ${multiidsget.length === 0
-                            ? "opacity-50 cursor-not-allowed"
-                            : "cursor-pointer"
-                            }`}
-                        onClick={multideletecourse}
-                        disabled={multiidsget.length === 0}
-                    >
-                        <span className="font-semibold">Multiple</span>
-                        <span className="font-semibold">Delete</span>
-                    </button>
 
+                    <h1 className="text-center text-[blue] text-[30px] py-[5px]">Pagination node.js api</h1>
                     <table className="min-w-full divide-y divide-gray-200">
                         <thead className="bg-gray-50">
                             <tr>
@@ -181,28 +75,15 @@ export default function Course_view() {
                                 <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
                                     description
                                 </th>
-                                <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    order
-                                </th>
-                                <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    delete
-                                </th>
-                                <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    status
-                                </th>
+
+
                             </tr>
                         </thead>
                         <tbody className="bg-white divide-y divide-gray-200">
                             {courses.length > 0 ? (
                                 courses.map((value, index) => (
                                     <tr key={index}>
-                                        <td>
-                                            <input
-                                                type="checkbox"
-                                                checked={multiidsget.includes(value._id)}
-                                                onChange={() => multiselect(value._id)}
-                                            />
-                                        </td>
+                                        <td></td>
                                         <td className="px-6 py-4 whitespace-nowrap text-sm text-center font-medium text-gray-900">
                                             {(pageNumber - 1) * limit + index + 1}
                                         </td>
@@ -227,38 +108,11 @@ export default function Course_view() {
                                             ${value.order}
                                         </td>
                                         <td className="px-6 py-4 whitespace-nowrap text-sm text-center text-gray-500">
-                                            <div className="flex justify-center space-x-4">
-                                                <button
-                                                    className="bg-red-600 text-white px-4 py-2 rounded-md shadow-md hover:bg-red-700 transition duration-300"
-                                                    onClick={() => deletecourse(value._id)}
-                                                >
-                                                    <FaTrash className="mr-2" />
-                                                    delete
-                                                </button>
-                                                <Link href={`/course_add/${value._id}`}>
-                                                    <button className="bg-blue-600 text-white px-4 py-2 rounded-md shadow-md hover:bg-blue-700 transition duration-300 flex items-center">
-                                                        <FaEdit className="mr-2" /> Edit
-                                                    </button>
-                                                </Link>
-                                            </div>
+
                                         </td>
 
                                         <td className="px-6 py-4 whitespace-nowrap text-center">
-                                            {value.status == 1 ? (
-                                                <button
-                                                    className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600"
-                                                    onClick={() => statuschange(value._id, value.status)}
-                                                >
-                                                    Active
-                                                </button>
-                                            ) : (
-                                                <button
-                                                    className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600"
-                                                    onClick={() => statuschange(value._id, value.status)}
-                                                >
-                                                    Inactive
-                                                </button>
-                                            )}
+
                                         </td>
                                     </tr>
                                 ))
